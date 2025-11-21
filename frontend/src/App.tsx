356a670e359778d7a5ce4aeade9b7340b6ca4f36
cench-dev <joystick1980@gmail.com>
2025-11-22 00:14:31 +0400

    Подключил фильтры к списку маршрутов

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import MapComponent from './components/map/mapComponent';
 import Navigation from './components/Header/Navigation';
 import './App.css';
-import FiltersPanel from './components/Filters/FiltersPanel';
+
 import RoutesPanel from './components/RoutesPanel/RoutesPanel';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <FiltersPanel />
+
       <MapComponent />
       <RoutesPanel />
     </div>
